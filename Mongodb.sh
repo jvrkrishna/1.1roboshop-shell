a9038fdca7c8d0f24d314d250c8234d641765850
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-28 20:59:23 +0530

    Frontend script

--- Mongodb.sh
+++ Mongodb.sh
@@ -3,7 +3,7 @@ cp Mongo.repo  /etc/yum.repos.d/mongodb.repo &>>/tmp/roboshop.log
 echo -e "\e[33mInstalling Mongodb Server\e[0m"
 yum install mongodb-org -y  &>>/tmp/roboshop.log
 echo -e "\e[33mUpdate Listen address\e[0m"
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[33mEnabling and starting Mongodb server\e[0m"
 systemctl enable mongod
 systemctl start mongod
