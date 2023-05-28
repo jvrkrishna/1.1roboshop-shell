84fb34a04f87dd2b5151b477e5d9b29c1c141211
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-28 22:11:56 +0530

    Frontend script

--- catalogue.sh
+++ catalogue.sh
@@ -1,16 +1,27 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+echo -e "\e[33mConfiguring Nodejs Server\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
+echo -e "\e[33mInstalling  Nodejs Server\e[0m"
+yum install nodejs -y &>> /tmp/roboshop.log
+echo -e "\e[33mAdding user\e[0m"
 useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+echo -e "\e[33mMake directory\e[0m"
+mkdir /app &>> /tmp/roboshop.log
+echo -e "\e[33mChanging code Server\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
+echo -e "\e[33mUnzipping code\e[0m"
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>> /tmp/roboshop.log
 cd /app
-npm install
-cp catalogue.service /etc/systemd/system/catalogue.service
+echo -e "\e[33mInstalling Dependencies\e[0m"
+npm install &>> /tmp/roboshop.log
+echo -e "\e[33mConfiguring Service file\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
 systemctl daemon-reload
-cp Mongo.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.rkdevops.store </app/schema/catalogue.js
+echo -e "\e[33mInstalling repo file\e[0m"
+cp Mongo.repo /etc/yum.repos.d/mongodb.repo &>> /tmp/roboshop.log
+yum install mongodb-org-shell -y &>> /tmp/roboshop.log
+echo -e "\e[33mConfiguring host\e[0m"
+mongo --host mongodb-dev.rkdevops.store </app/schema/catalogue.js &>> /tmp/roboshop.log
+echo -e "\e[33mEnabling and restarting Server\e[0m"
 systemctl enable catalogue
 systemctl restart catalogue
