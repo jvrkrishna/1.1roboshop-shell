f57c20d4e08514ee08abefc5f9459a0c25f4fc9f
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-29 21:23:29 +0530

    Frontend script

--- redis.sh
+++ redis.sh
@@ -1,5 +1,5 @@
 echo -e "\e[33mInstalling Redis server\e[0m"
-yum install redis -y
+yum install redis -y &>>/tmp/roboshop.log
 echo -e "\e[33mModifying conf file\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf
 echo -e "\e[33mEnabling and restarting redis server\e[0m"
