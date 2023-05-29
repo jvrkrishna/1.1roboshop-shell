8dffe4ad3138d7e3f44eb9f837a44e13479db22d
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-29 21:22:05 +0530

    Frontend script

--- redis.sh
+++ redis.sh
@@ -4,4 +4,4 @@ echo -e "\e[33mModifying conf file\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/'  /etc/redis.conf
 echo -e "\e[33mEnabling and restarting redis server\e[0m"
 systemctl enable redis
-systemcl restart redis
+systemctl restart redis
