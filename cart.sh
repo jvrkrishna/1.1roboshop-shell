1ed10a99cfbac5cbcfe2c0f35cafedfe3405d208
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-29 22:56:26 +0530

    Frontend script

--- cart.sh
+++ cart.sh
@@ -10,6 +10,7 @@ cd /app
 unzip /tmp/cart.zip &>>/tmp/roboshop.log
 cd /app
 echo -e '\e[33mInstalling dependencies\e[30m'
+curl -qL https://www.npmjs.com/install.sh | sh
 npm install
 echo -e '\e[33mcreating service file\e[30m'
 cp cart.service /etc/systemd/system/cart.service &>>/tmp/roboshop.log
