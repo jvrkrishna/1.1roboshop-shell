d8d43d7273467ef7e02d30da08be21f2f70d0016
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-30 12:23:56 +0530

    shipping script

--- shipping.sh
+++ shipping.sh
@@ -4,20 +4,20 @@ echo -e "\e[33mCreating user\e[0m"
 useradd roboshop
 echo -e "\e[33mCopying New app content\e[0m"
 mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
 cd /app
 unzip /tmp/shipping.zip
 cd /app
 echo -e "\e[33mcleaning maven package\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
+mvn clean package &>>/tmp/roboshop.log
+mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
 echo -e "\e[33mCopying service file\e[0m"
-cp Shipping.Service /etc/systemd/system/shipping.service
-systemctl daemon-reload
+cp shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
+systemctl daemon-reload &>>/tmp/roboshop.log
 echo -e "\e[33mEnabling and restating the service\e[0m"
-systemctl enable shipping
+systemctl enable shipping &>>/tmp/roboshop.log
 systemctl restart shipping
 echo -e "\e[33mLoading mysql schema\e[0m"
-yum install mysql -y
-mysql -h mysql-dev.rkdevops.store -uroot -pRoboShop@1 < /app/schema/shipping.sql
+yum install mysql -y &>>/tmp/roboshop.log
+mysql -h mysql-dev.rkdevops.store -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
 systemctl restart shipping
