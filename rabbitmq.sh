cdfa7572cddaa42f668aaf8b832a48314dfc0811
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-05-30 21:15:48 +0530

    shipping script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,6 +6,6 @@ yum install rabbitmq-server -y &>> /tmp/roboshop.log
 echo -e "\e[33mEnabling and restarting rabbitmq\e[0m"
 systemctl enable rabbitmq-server &>> /tmp/roboshop.log
 systemctl restart rabbitmq-server &>> /tmp/roboshop.log
-echo -e "\e[33mAdding user and setting peermissions\e[0m"
+echo -e "\e[33mAdding user and setting permissions\e[0m"
 rabbitmqctl add_user roboshop roboshop123 &>> /tmp/roboshop.log
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> /tmp/roboshop.log
