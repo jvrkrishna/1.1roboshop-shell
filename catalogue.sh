03112de18a3c87881cab686f0266c7eed8d8cb36
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-01 11:43:22 +0530

    DevOps321

--- catalogue.sh
+++ catalogue.sh
@@ -1,3 +1,4 @@
+component=catalogue
 echo -e "\e[33mConfiguring Nodejs Server\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
 echo -e "\e[33mInstalling  Nodejs Server\e[0m"
@@ -7,21 +8,21 @@ useradd roboshop
 echo -e "\e[33mMake directory\e[0m"
 mkdir /app &>> /tmp/roboshop.log
 echo -e "\e[33mChanging code Server\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> /tmp/roboshop.log
 echo -e "\e[33mUnzipping code\e[0m"
 cd /app
-unzip /tmp/catalogue.zip &>> /tmp/roboshop.log
+unzip /tmp/$component.zip &>> /tmp/roboshop.log
 cd /app
 echo -e "\e[33mInstalling Dependencies\e[0m"
 npm install &>> /tmp/roboshop.log
 echo -e "\e[33mConfiguring Service file\e[0m"
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>> /tmp/roboshop.log
 systemctl daemon-reload
 echo -e "\e[33mInstalling repo file\e[0m"
 cp /home/centos/roboshop-shell/Mongo.repo /etc/yum.repos.d/mongodb.repo &>> /tmp/roboshop.log
 yum install mongodb-org-shell -y &>> /tmp/roboshop.log
 echo -e "\e[33mConfiguring host\e[0m"
-mongo --host mongodb-dev.rkdevops.store </app/schema/catalogue.js &>> /tmp/roboshop.log
+mongo --host mongodb-dev.rkdevops.store </app/schema/$component.js &>> /tmp/roboshop.log
 echo -e "\e[33mEnabling and restarting Server\e[0m"
-systemctl enable catalogue &>> /tmp/roboshop.log
-systemctl restart catalogue
+systemctl enable $component &>> /tmp/roboshop.log
+systemctl restart $component
