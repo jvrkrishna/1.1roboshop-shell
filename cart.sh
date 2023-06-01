5986ab348ada77d50f0f841cceba2eebd3f5bfd9
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-01 13:34:56 +0530

    DevOps321

--- cart.sh
+++ cart.sh
@@ -1,23 +1,23 @@
 source common.sh
 component=cart
 
-echo -e '${color}Downloading Nodejs${nocolor}'
+echo -e "${color}Downloading Nodejs${nocolor}"
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash - &>>${logfile}
-echo -e '${color}Installing Nodejs${nocolor}'
+echo -e "${color}Installing Nodejs${nocolor}"
 yum install nodejs -y &>>${logfile}
-echo -e '${color}Creating user${nocolor}'
+echo -e "${color}Creating user${nocolor}"
 useradd roboshop
 mkdir ${app_path}
 cd ${app_path}
-echo -e '${color}Downloading and unzipping the new app content${nocolor}'
+echo -e "${color}Downloading and unzipping the new app content${nocolor}"
 curl -L -o ${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${logfile}
 unzip ${component}.zip &>>${logfile}
-echo -e '${color}Installing dependencies${nocolor}'
+echo -e "${color}Installing dependencies${nocolor}"
 npm install &>>${logfile}
-echo -e '${color}Creating service file${nocolor}'
+echo -e "${color}Creating service file${nocolor}"
 cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${logfile}
 systemctl daemon-reload &>>${logfile}
-echo -e '${color}Enabling and restarting server${nocolor}'
+echo -e "${color}Enabling and restarting server${nocolor}"
 systemctl enable ${component} &>>${logfile}
 systemctl restart ${component} &>>${logfile}
 
