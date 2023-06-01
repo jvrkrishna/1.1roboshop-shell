ee4c0903cb6760d4563ba2317900d76c22f55e76
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-01 12:56:07 +0530

    DevOps321

--- catalogue.sh
+++ catalogue.sh
@@ -1,31 +1,32 @@
 component=catalogue
 color="\e[36m"
 nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
 
 echo -e "${color}Configuring Nodejs Server${nocolor}"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> ${logfile}
 echo -e "${color}Installing  Nodejs Server${nocolor}"
-yum install nodejs -y &>> /tmp/roboshop.log
+yum install nodejs -y &>> ${logfile}
 echo -e "${color}Adding user${nocolor}"
 useradd roboshop
 echo -e "${color}Make directory${nocolor}"
-mkdir /app &>> /tmp/roboshop.log
+mkdir /app &>> ${logfile}
 echo -e "${color}Changing code Server${nocolor}"
-curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> /tmp/roboshop.log
+curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> ${logfile}
 echo -e "${color}Unzipping code${nocolor}"
 cd /app
-unzip /tmp/$component.zip &>> /tmp/roboshop.log
+unzip /tmp/$component.zip &>> ${logfile}
 cd /app
 echo -e "${color}Installing Dependencies${nocolor}"
-npm install &>> /tmp/roboshop.log
+npm install &>> ${logfile}
 echo -e "${color}Configuring Service file${nocolor}"
-cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>> /tmp/roboshop.log
+cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>> ${logfile}
 systemctl daemon-reload
 echo -e "${color}Installing repo file${nocolor}"
-cp /home/centos/roboshop-shell/Mongo.repo /etc/yum.repos.d/mongodb.repo &>> /tmp/roboshop.log
-yum install mongodb-org-shell -y &>> /tmp/roboshop.log
+cp /home/centos/roboshop-shell/Mongo.repo /etc/yum.repos.d/mongodb.repo &>> ${logfile}
+yum install mongodb-org-shell -y &>> ${logfile}
 echo -e "${color}Configuring host${nocolor}"
-mongo --host mongodb-dev.rkdevops.store </app/schema/$component.js &>> /tmp/roboshop.log
+mongo --host mongodb-dev.rkdevops.store </app/schema/$component.js &>> ${logfile}
 echo -e "${color}Enabling and restarting Server${nocolor}"
-systemctl enable $component &>> /tmp/roboshop.log
+systemctl enable $component &>> ${logfile}
 systemctl restart $component
