6af925e0bdebe316ad5af2af7eb4bc31f8030e30
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 15:22:09 +0530

    DevOps321

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,15 @@
 source common.sh
 
-echo -e "${color}Installing Nginx Server${nocolor}"
+echo -e "${color}Copying repo${nocolor}"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${logfile}
 status $?
-echo -e "${color}Installing Nginx Server${nocolor}"
+echo -e "${color}Installing Mongodb Server${nocolor}"
 yum install mongodb-org -y &>>${logfile}
 status $?
-echo -e "${color}Installing Nginx Server${nocolor}"
+echo -e "${color}Configuring localhost File${nocolor}"
 sed 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${logfile}
 status $?
-echo -e "${color}Installing Nginx Server${nocolor}"
+echo -e "${color}Enabling and restarting Mongodb Server${nocolor}"
 systemctl enable mongod &>>${logfile}
 status $?
 systemctl start mongod
