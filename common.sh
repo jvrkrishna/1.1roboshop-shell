c999374a63fa9898c8d730d86be7c1d080a74b00
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 09:24:28 +0530

    DevOps321

--- common.sh
+++ common.sh
@@ -6,21 +6,52 @@ app_path="/app"
 app_presetup(){
     echo -e "${color}Adding user${nocolor}"
     useradd roboshop
+    if [ $? -eq 0 ]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
+
     echo -e "${color}Make directory${nocolor}"
     mkdir ${app_path} &>> ${logfile}
+     if [ $? -eq 0 ]; then
+          echo SUCCESS
+        else
+          echo FAILURE
+        fi
     echo -e "${color}Downloading the new content to the Server${nocolor}"
     curl -o /tmp/$component.zip https://roboshop-artifacts.s3.amazonaws.com/$component.zip &>> ${logfile}
+     if [ $? -eq 0 ]; then
+          echo SUCCESS
+        else
+          echo FAILURE
+        fi
     echo -e "${color}Unzipping code${nocolor}"
     cd ${app_path}
     unzip /tmp/$component.zip &>> ${logfile}
+     if [ $? -eq 0 ]; then
+          echo SUCCESS
+        else
+          echo FAILURE
+        fi
 }
 
 systemd_setup(){
   echo -e "${color}Configuring Service file${nocolor}"
   cp /home/centos/roboshop-shell/$component.service /etc/systemd/system/$component.service &>> ${logfile}
+   if [ $? -eq 0 ]; then
+        echo SUCCESS
+      else
+        echo FAILURE
+      fi
   systemctl daemon-reload
   echo -e "${color}Enabling and restarting Server${nocolor}"
   systemctl enable $component &>> ${logfile}
+   if [ $? -eq 0 ]; then
+        echo SUCCESS
+      else
+        echo FAILURE
+      fi
   systemctl restart $component
 }
 
