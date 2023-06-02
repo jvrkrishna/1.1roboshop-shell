254ad34b5b5b2ec60425d2d18ea09fb30b4b8845
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 22:11:40 +0530

    DevOps321

--- mongodb.sh
+++ mongodb.sh
@@ -10,5 +10,7 @@ status $?
 echo -e "${color}Configuring localhost File${nocolor}"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf&>>${logfile}
 status $?
+echo -e "${color}Enabling and restarting the service${nocolor}"
 systemctl enable mongod
 systemctl restart mongod
+status $?
