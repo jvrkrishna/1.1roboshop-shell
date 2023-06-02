ff181afdecf1f175acc6c49fdc0d09a9084a67b2
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 15:34:01 +0530

    DevOps321

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 echo -e "${color}Copying repo${nocolor}"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${logfile}
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>${logfile}
 status $?
 echo -e "${color}Installing Mongodb Server${nocolor}"
 yum install mongodb-org -y &>>${logfile}
