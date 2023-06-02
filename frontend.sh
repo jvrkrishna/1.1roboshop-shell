8512072d49ccb575dfd8d76f9c4bf0b041d601db
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 14:43:41 +0530

    DevOps321

--- frontend.sh
+++ frontend.sh
@@ -1,14 +1,18 @@
 source common.sh
 echo -e "${color}Installing Nginx Server${nocolor}"
 yum install nginx -y &>>${logfile}
+status
 echo -e "${color}Removing old content${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>${logfile}
+status
 echo -e "${color}Downloading new app content${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${logfile}
 cd /usr/share/nginx/html &>>${logfile}
 unzip /tmp/frontend.zip &>>${logfile}
+status
 echo -e "${color}Configuring Reverse proxy seerver${nocolor}"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${logfile}
+status
 echo -e "${color}Installing Nginx Server${nocolor}"
 systemctl enable nginx &>>${logfile}
 systemctl restart nginx
