a82c2af7470b80935276eead0d108c0ac706e414
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-02 14:38:43 +0530

    DevOps321

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,14 @@
 source common.sh
-
-echo -e "\e[31mInstalling Nginx Server\e[0m"
-yum install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[31mRemoving old content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
-echo -e "\e[31mDownloading new app content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
-cd /usr/share/nginx/html &>>/tmp/roboshop.log
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
-echo -e "\e[31mConfiguring Reverse proxy seerver\e[0m"
-cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
-echo -e "\e[31mInstalling Nginx Server\e[0m"
-systemctl enable nginx &>>/tmp/roboshop.log
+echo -e "${color}Installing Nginx Server${nocolor}"
+yum install nginx -y &>>${logfile}
+echo -e "${color}Removing old content${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>${logfile}
+echo -e "${color}Downloading new app content${nocolor}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${logfile}
+cd /usr/share/nginx/html &>>${logfile}
+unzip /tmp/frontend.zip &>>${logfile}
+echo -e "${color}Configuring Reverse proxy seerver${nocolor}"
+cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${logfile}
+echo -e "${color}Installing Nginx Server${nocolor}"
+systemctl enable nginx &>>${logfile}
 systemctl restart nginx
