af9057e4be0e1cd8e0f4f514150ae0df2a169f5b
ramakrishnajunnu <ramakrishnajunnu@gmail.com>
2023-06-07 14:36:05 +0530

    playbook

--- common.sh
+++ common.sh
@@ -59,7 +59,7 @@ mongo_schema(){
   status $?
   echo -e "${color}Installing mongo schema${nocolor}"
   yum install mongodb-org-shell -y &>>${logfile}
-  mongo --host 172.31.13.233 </app/schema/${component}.js &>>${logfile}
+  mongo --host mongodb-dev.rkdevops.store </app/schema/${component}.js &>>${logfile}
   status $?
 }
 
