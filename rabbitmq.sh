8f8c4bd7b93aa2ee80e2dd1487ed95310e40b099
jvrkrishna <132902600+jvrkrishna@users.noreply.github.com>
2023-10-05 19:54:38 +0530

    Update rabbitmq.sh

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,3 +5,19 @@ systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 rabbitmqctl add_user roboshop roboshop123
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+component=rabbitmq
+source common.sh
+
+echo -e "${color}Creating ${component} repo${nocolor}"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${logfile}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${logfile}
+status $?
+echo -e "${color}Installing ${component} Server${nocolor}"
+yum install rabbitmq-server -y &>>${logfile}
+status $?
+echo -e "${color}Setting Permissions to Default user${nocolor}"
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+status $?
+service_start
